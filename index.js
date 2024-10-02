8c90a89e0c259bba672402e7a158f3c1c05ccd64
zephania owuor <ularezephaniah@gmail.com>
2024-10-02 21:19:35 +0300

    method-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(record) {
+
+const superbowlWin=record.find(win=>
+ win.result ==="W");
+ return superbowlWin? superbowlWin.year : undefined;
+ 
+}
+console.log(superbowlWin);
